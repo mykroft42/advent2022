f2cecc48cdf792bd9f2410057768ee777a88a9f8
Matt Kemmerer <mykroft@gmail.com>
2022-12-01 20:28:16 -0500

    Day 1 complete

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var lines = File.ReadAllLines("input.txt").ToList();
+List<List<int>> cals = new List<List<int>>();
+var current = new List<int>();
+cals.Add(current);
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        current = new List<int>();
+        cals.Add(current);
+        continue;
+    }
+
+    current.Add(int.Parse(line));
+}
+
+var answer = cals.Select(c => c.Sum()).OrderByDescending(c => c).Take(3).Sum();
+Console.WriteLine(answer);
