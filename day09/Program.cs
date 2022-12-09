544499ddfd1d52b6853f9bc6d42062e4023ce49b
Matthew Kemmerer <matthew.kemmerer@flexential.com>
2022-12-09 12:51:07 -0500

    Completed day 9  part 2

--- day09/Program.cs
+++ day09/Program.cs
@@ -9,8 +9,21 @@ namespace day09
     {
         static void Main(string[] args)
         {
-            (int x, int y) head = (0, 0);
-            (int x, int y) tail = (0, 0);
+            // (int x, int y) head = (0, 0);
+            // (int x, int y) tail = (0, 0);
+            List<(int x, int y)> knots = new List<(int x, int y)>()
+            {
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+                (0,0),
+            };
 
             List<(int x, int y)> tails = new List<(int x, int y)> { (0, 0) };
 
@@ -25,54 +38,61 @@ namespace day09
                     switch(comps[0])
                     {
                         case "R":
-                            head = (head.x + 1, head.y);
+                            knots[0] = (knots[0].x + 1, knots[0].y);
                             break;
                         case "L":
-                            head = (head.x - 1, head.y);
+                            knots[0] = (knots[0].x - 1, knots[0].y);
                             break;
                         case "U":
-                            head = (head.x, head.y + 1);
+                            knots[0] = (knots[0].x, knots[0].y + 1);
                             break;
                         case "D":
-                            head = (head.x, head.y - 1);
+                            knots[0] = (knots[0].x, knots[0].y - 1);
                             break;
                     }
 
-                    if (Math.Abs(head.x - tail.x) <= 1 && Math.Abs(head.y - tail.y) <= 1)
+                    for (int j = 0; j < knots.Count - 1; j++)
                     {
-                        continue;
-                    }
-
-                    if (Math.Abs(head.x - tail.x) > 1)
-                    {
-                        if (head.x - tail.x > 0) tail = (tail.x + 1, tail.y);
-                        else tail = (tail.x - 1, tail.y);
-                        
-                        if (head.y != tail.y)
-                        {
-                            if (head.y - tail.y > 0) tail = (tail.x, tail.y + 1);
-                            else tail = (tail.x, tail.y - 1);
-                        }
-
-                        tails.Add(tail);
-                    }
-                    else
-                    {
-                        if (head.y - tail.y > 0) tail = (tail.x, tail.y + 1);
-                        else tail = (tail.x, tail.y - 1);
-                        
-                        if (head.x != tail.x)
-                        {
-                            if (head.x - tail.x > 0) tail = (tail.x + 1, tail.y);
-                            else tail = (tail.x - 1, tail.y);
-                        }
-
-                        tails.Add(tail);
-                    }
+                        var tail = knots[j+1];
+                        processMove(knots[j], ref tail);
+                        knots[j+1] = tail;
+                    }                    
+                    tails.Add(knots.Last());
                 }
             }
 
             Console.WriteLine(tails.Distinct().Count());
+        }
+
+        static void processMove((int x, int y) head, ref (int x, int y) tail)
+        {
+            if (Math.Abs(head.x - tail.x) <= 1 && Math.Abs(head.y - tail.y) <= 1)
+            {
+                return;
+            }
+
+            if (Math.Abs(head.x - tail.x) > 1)
+            {
+                if (head.x - tail.x > 0) tail = (tail.x + 1, tail.y);
+                else tail = (tail.x - 1, tail.y);
+                
+                if (head.y != tail.y)
+                {
+                    if (head.y - tail.y > 0) tail = (tail.x, tail.y + 1);
+                    else tail = (tail.x, tail.y - 1);
+                }
+            }
+            else
+            {
+                if (head.y - tail.y > 0) tail = (tail.x, tail.y + 1);
+                else tail = (tail.x, tail.y - 1);
+                
+                if (head.x != tail.x)
+                {
+                    if (head.x - tail.x > 0) tail = (tail.x + 1, tail.y);
+                    else tail = (tail.x - 1, tail.y);
+                }
+            }
         }
     }
 }
