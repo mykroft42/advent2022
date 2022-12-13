75efb76379b8530b4249a48fa24f32d85fb87a57
Matt Kemmerer <mykroft@gmail.com>
2022-12-13 09:12:11 -0500

    completed day12

---
+++ day12/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day12
+{
+    internal class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public char height
+        {
+            get
+            {
+                if (RealHeight == 'S') return 'a';
+                else if (RealHeight == 'E') return 'z';
+                else return RealHeight;
+            }
+            set
+            {
+                RealHeight = value;
+            }
+        }
+
+        public char RealHeight { get; set; }
+
+        public List<Node> Neighbors { get; set; } = new List<Node>();
+    }
+}
---
+++ day12/Program.cs
@@ -0,0 +1,45 @@
+﻿using day12;
+using Graphalo;
+using Graphalo.Traversal;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("input.txt");
+
+        List<Node> nodes = new List<Node>();
+
+        int x = 0, y = 0;
+        foreach (var line in lines)
+        {
+            foreach(var node in line)
+            {
+                Node item = new Node { X = x, Y = y, height = node };
+                nodes.Add(item);
+                x++;
+            }
+            y++;
+            x = 0;
+        }
+
+        nodes.ForEach(item => item.Neighbors = nodes.Where(n =>
+                    ((Math.Abs(n.X - item.X) == 1 && n.Y == item.Y) ||
+                     (Math.Abs(n.Y - item.Y) == 1 && n.X == item.X)) &&
+                    ((item.height >= n.height) || (n.height - item.height == 1))).ToList());
+
+        DirectedGraph<Node> graph = new DirectedGraph<Node>();
+        graph.AddEdges(nodes.SelectMany(n => n.Neighbors.Select(nn => new Edge<Node>(n, nn))));
+
+        //var result = graph.Traverse(Graphalo.Traversal.TraversalKind.Dijkstra, nodes.Single(n => n.RealHeight == 'S'), nodes.Single(n => n.RealHeight == 'E'));
+        Node end = nodes.Single(n => n.RealHeight == 'E');
+        int steps = int.MaxValue;
+        foreach (var item in nodes.Where(n => n.height == 'a'))
+        {
+            var result = graph.Traverse(TraversalKind.Dijkstra, item, end);
+            if (result.Success && (result.Results.Count() - 1) < steps) steps = result.Results.Count() - 1;
+        }
+
+        Console.WriteLine(steps);
+    }
+}
